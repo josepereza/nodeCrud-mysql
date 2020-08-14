cf6a016db240910a340fbfbbd89478ea42945115
soumitracode <psoumitra22@gmail.com>
2020-08-14 20:13:56 +0530

    morgan an http loger is added in dev dependency

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
-var cors = require('cors')
 const app = express()
+
+var cors = require('cors')
 app.use(cors())
 
+var morgan = require('morgan');
+app.use(morgan('tiny'))
 
 var bodyParser = require('body-parser')
- 
-
- 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
 // parse application/json
 app.use(bodyParser.json())
 
-const mysql = require('mysql');
 
 const routerr = require('./routers/user.js');
 app.use(routerr);
@@ -24,7 +22,8 @@ app.use(express.static('./public'));
 app.listen(3000,function(req,res){
     console.log('server is running');
 });
-app.get('/users',(req,res)=>{
+
+app.get('/test',(req,res)=>{
     var one = {firstname:"sanjoy",lastname:"giri"};
     var two = {firstname:"soumitra",lastname:"patra"};
     res.json([one ,two]);
